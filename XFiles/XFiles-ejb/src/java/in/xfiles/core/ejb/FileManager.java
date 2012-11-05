aab144a050f9f37fb222512ea5a103b2c0f555dd
danon.oren@gmail.com <danon.oren@gmail.com@5ead5ee2-ed5d-3a2b-4562-3cd651b19626>
2012-11-05 10:41:35 +0000

    Created servlet for uploading files.
    TODO: FileManager implementation

--- XFiles/XFiles-ejb/src/java/in/xfiles/core/ejb/FileManager.java
+++ XFiles/XFiles-ejb/src/java/in/xfiles/core/ejb/FileManager.java
@@ -1,10 +1,9 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package in.xfiles.core.ejb;
 
+import in.xfiles.core.wrappers.UploadedFileWrapper;
+import javax.ejb.Asynchronous;
 import javax.ejb.Stateless;
+import org.apache.log4j.Logger;
 
 /**
  *
@@ -12,5 +11,16 @@ import javax.ejb.Stateless;
  */
 @Stateless
 public class FileManager implements FileManagerLocal{
+
+    private Logger log = Logger.getLogger(FileManager.class);
+    
+    @Override @Asynchronous
+    public void processFile(UploadedFileWrapper ufw) {
+        log.debug("Started asynchronous processing of uploaded file: "+ufw);
+        // TODO: implementation
+        
+        // Delete temporary file after processing
+        ufw.getFile().delete();
+    }
     
 }
---
+++ XFiles/XFiles-ejb/src/java/in/xfiles/core/wrappers/UploadedFileWrapper.java
@@ -0,0 +1,91 @@
+package in.xfiles.core.wrappers;
+
+import in.xfiles.core.entity.User;
+import java.io.File;
+import java.io.Serializable;
+
+/**
+ * Aggregates all necessary information about uploaded file.
+ * 
+ * @author danon
+ */
+public class UploadedFileWrapper implements Serializable {
+    
+    private File file;
+    private String name;
+    private String contentType;
+    private String accessType;
+    private String encryptionType;
+    private long size;
+    private long checksum;
+    private User uploadedBy;
+
+    public File getFile() {
+        return file;
+    }
+
+    public void setFile(File f) {
+        this.file = f;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    public String getAccessType() {
+        return accessType;
+    }
+
+    public void setAccessType(String accessType) {
+        this.accessType = accessType;
+    }
+
+    public String getEncryptionType() {
+        return encryptionType;
+    }
+
+    public void setEncryptionType(String encryptionType) {
+        this.encryptionType = encryptionType;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public void setSize(long size) {
+        this.size = size;
+    }
+
+    public long getChecksum() {
+        return checksum;
+    }
+
+    public void setChecksum(long checksum) {
+        this.checksum = checksum;
+    }
+
+    public User getUploadedBy() {
+        return uploadedBy;
+    }
+
+    public void setUploadedBy(User uploadedBy) {
+        this.uploadedBy = uploadedBy;
+    }
+
+    @Override
+    public String toString() {
+        return "UploadedFileWrapper{" + "file=" + file + ", name=" + name + ", contentType=" + contentType + ", accessType=" + accessType + ", encryptionType=" + encryptionType + ", size=" + size + ", checksum=" + checksum + ", uploadedBy=" + uploadedBy + '}';
+    }
+}
