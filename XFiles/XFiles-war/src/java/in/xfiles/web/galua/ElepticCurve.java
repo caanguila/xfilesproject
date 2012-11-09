bb8ac1fa9e97ec549a3496c5d4b50f2e406c40af
out310@gmail.com <out310@gmail.com@5ead5ee2-ed5d-3a2b-4562-3cd651b19626>
2012-11-09 21:50:05 +0000

    Some calculations on galua fild

---
+++ XFiles/XFiles-war/src/java/in/xfiles/web/galua/ElepticCurve.java
@@ -0,0 +1,49 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package in.xfiles.web.galua;
+
+/**
+ *
+ * @author 7
+ */
+public class ElepticCurve {
+    
+    public static long[] summ(long[] a, long[] b, long n, long paramA, long paramB){
+    long[] c = new long[2];
+    long s=0;
+    if(a[0] == b[0] && a[1]!=b[1]){
+        c[0] = Long.MAX_VALUE;
+        c[1] = Long.MAX_VALUE;
+        return c;
+    }
+    if(a[0] == b[0] && a[1] ==b[1]){
+         s = ((3*a[0]*a[0] + paramA) * (UsefulAlgo.ReverseElement(2*a[1], n)))%n;
+         
+        if(s<0) s +=n;
+        c[0] = (s*s - a[0] - b[0])%n;
+        c[1] = (a[1] -s*(a[0] - c[0]))%n;
+        if(c[0]<0) c[0]+=n;
+        if(c[1]<0) c[1]+=n;
+        System.out.println("s:"+s);
+        System.out.println("x3:"+c[0]);
+        System.out.println("y3:"+c[1]);
+        return c;
+    }
+    if(a[0] != b[0] && a[1]!=b[1]){
+        s = ((b[1] - a[1]) * (UsefulAlgo.ReverseElement(b[0]-a[0], n)))%n;
+        if(s<0) s +=n;
+        c[0] = (s*s - a[0] - b[0])%n;
+        c[1] = (a[1] -s*(a[0] - c[0]))%n;
+        if(c[0]<0) c[0]+=n;
+        if(c[1]<0) c[1]+=n;
+        
+      //  System.out.println("s:"+s);
+      //  System.out.println("x3:"+c[0]);
+      //  System.out.println("y3:"+c[1]);
+        return c;
+    }
+    return c;
+    }
+}
