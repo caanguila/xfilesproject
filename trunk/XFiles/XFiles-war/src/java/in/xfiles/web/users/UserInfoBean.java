7d7805f772373e905b02850034381c623c687c9a
out310@gmail.com <out310@gmail.com@5ead5ee2-ed5d-3a2b-4562-3cd651b19626>
2012-11-12 21:16:14 +0000

    Created cabinet of user; But some serious bugs are not fixed

---
+++ trunk/XFiles/XFiles-war/src/java/in/xfiles/web/users/UserInfoBean.java
@@ -0,0 +1,136 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package in.xfiles.web.users;
+
+import in.xfiles.core.ejb.UserManager;
+import in.xfiles.core.ejb.UserManagerLocal;
+import in.xfiles.core.entity.User;
+import in.xfiles.web.utils.JSFHelper;
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import java.util.Date;
+import javax.ejb.EJB;
+import javax.faces.event.ActionEvent;
+import org.apache.log4j.Logger;
+/**
+ *
+ * @author 7
+ */
+@ManagedBean
+@ViewScoped
+public class UserInfoBean implements Serializable{
+    private final Logger log = Logger.getLogger(UserInfoBean.class);
+    
+    @EJB
+    UserManagerLocal userManager;
+    
+    public Long userId;
+    public String userName;
+    public String surName;
+    public Date dateCreation;
+    public Date dateSuspend;
+    public String email;
+    public String information;
+    private User user;
+    /**
+     * Creates a new instance of UserInfoBean
+     */
+    public UserInfoBean() {
+       userId = JSFHelper.getUserId();
+    }
+
+    private void checkUserEntity(){
+        
+        if(userId != null && user == null){
+            user = userManager.getUserById(userId);
+            
+        }else{
+           log.warn("User Id is "+userId); 
+        }
+    }
+    
+    public void saveParametrsAction(ActionEvent e){
+        log.info("Save parametr Action");
+        log.info(userName);
+        log.info(surName);
+        log.info(dateCreation);
+        log.info(dateSuspend);
+        log.info(email);
+        log.info(information);
+        log.info(userId);
+    }
+    
+    public Date getDateCreation() {
+        checkUserEntity();
+        dateCreation = user.getDateCreation();
+        return dateCreation;
+    }
+
+    public void setDateCreation(Date dateCreation) {
+        this.dateCreation = dateCreation;
+    }
+
+    public Date getDateSuspend() {
+        checkUserEntity();
+        dateSuspend = user.getDateSuspended();
+        return dateSuspend;
+    }
+
+    public void setDateSuspend(Date dateSuspend) {
+        this.dateSuspend = dateSuspend;
+    }
+
+    public String getEmail() {
+        checkUserEntity();
+        email = user.getEmail();
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getInformation() {
+        checkUserEntity();
+        information = user.getInformation();
+        return information;
+    }
+
+    public void setInformation(String information) {
+        this.information = information;
+    }
+
+    public String getSurName() {
+        checkUserEntity();
+        surName = user.getSurname();
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public String getUserName() {
+        checkUserEntity();
+        userName = user.getName();
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+    
+    
+    
+}
