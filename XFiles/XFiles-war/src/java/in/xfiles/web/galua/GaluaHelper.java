981e9396e62d5b851657fe6e70ae3ddd17209993
out310@gmail.com <out310@gmail.com@5ead5ee2-ed5d-3a2b-4562-3cd651b19626>
2012-11-16 21:33:31 +0000

    galua

--- XFiles/XFiles-war/src/java/in/xfiles/web/galua/GaluaHelper.java
+++ XFiles/XFiles-war/src/java/in/xfiles/web/galua/GaluaHelper.java
@@ -6,7 +6,7 @@ package in.xfiles.web.galua;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
-
+import java.util.*;
 /**
  *
  * @author 7
@@ -29,6 +29,13 @@ public class GaluaHelper {
     public long curveN;
     public long paramA;
     public long paramB;
+    
+    public long curve1N;
+    public long param1A;
+    public long param1B;
+    
+    public long elemX;
+    public long elemY;
     /**
      * Creates a new instance of GaluaHelper
      */
@@ -123,6 +130,30 @@ public class GaluaHelper {
         this.paramB = paramB;
     }
 
+    public long getCurve1N() {
+        return curve1N;
+    }
+
+    public void setCurve1N(long curve1N) {
+        this.curve1N = curve1N;
+    }
+
+    public long getParam1A() {
+        return param1A;
+    }
+
+    public void setParam1A(long param1A) {
+        this.param1A = param1A;
+    }
+
+    public long getParam1B() {
+        return param1B;
+    }
+
+    public void setParam1B(long param1B) {
+        this.param1B = param1B;
+    }
+
     
     
     
@@ -137,15 +168,36 @@ public class GaluaHelper {
     
     public long getReverseElement(){
         
+       if(n!=0) elementToReverse = ((elementToReverse % n)+n)%n;
         return UsefulAlgo.ReverseElement(elementToReverse, n);
     }
+    
     public long getPowerResult(){
-        return UsefulAlgo.qe2(elementToPower, power, n1);
+        if(n1!=0) elementToPower = ((elementToPower % n1)+n1)%n1;
+        
+        if(power > 0){
+          return UsefulAlgo.qe2(elementToPower, power, n1);
+        }
+        else if(power == 0){
+          return 1;
+        }else if(power < 0){
+          return UsefulAlgo.qe2(UsefulAlgo.ReverseElement(elementToPower, n1), -power, n1);
+        }
+        
+        return 0;
     }
     
     public String getSummOfCurve(){
         long[] point1 = new long[2];
         long[] point2 = new long[2];
+        
+        if(curveN!=0){
+            x1 = ((x1 % curveN)+curveN)%curveN;
+            y1 = ((y1 % curveN)+curveN)%curveN;
+            x2 = ((x2 % curveN)+curveN)%curveN;
+            y2 = ((y2 % curveN)+curveN)%curveN;
+             
+        }
         point1[0] = x1;
         point1[1] = y1;
         
@@ -154,7 +206,138 @@ public class GaluaHelper {
         if(curveN==0) return "";
         
         long[] point3 = ElepticCurve.summ(point1, point2, curveN, paramA, paramB);
-        return "( "+point3[0]+", "+point3[1]+" )";
+        return " ( "+point3[0]+", "+point3[1]+" ) ";
     }
     
+    public List yval; 
+    
+    public String getCurveValues(){
+        HashMap sqmap = new HashMap();
+        HashMap sqymap = new HashMap();
+        for(long i = 0; i< curve1N; i++){
+          long sqy = ((i*i*i)%curve1N + param1A * i +param1B)%curve1N;  
+          sqmap.put(i, (i*i)%curve1N);
+          sqymap.put(i, sqy);
+        }
+      //  System.out.println(getCurveMapElements(sqmap));
+       // System.out.println(getCurveMapElements(sqymap));
+        
+        yval = curveSqrt(sqmap, sqymap);//(x,y)
+    //    getAllSubGroups();
+//        if(!yval.isEmpty())
+//        getSubGroup((Long)yval.get(0), (Long)yval.get(1));
+        return getCurveMapElements(yval);
+    }
+    
+    public String getSubGroups(){
+        String result ="";
+        if(elemX !=0 || elemY!=0){
+            List subGroup = getSubGroup(elemX, elemY);
+            result+=" ( "+elemX+", "+elemY+" ): {"+getCurveMapElements(subGroup)+"} \n";
+            System.out.println("All Subgroups: "+result);
+        }
+        return result;
+    }
+
+    public long getElemX() {
+        return elemX;
+    }
+
+    public void setElemX(long elemX) {
+        this.elemX = elemX;
+    }
+
+    public long getElemY() {
+        return elemY;
+    }
+
+    public void setElemY(long elemY) {
+        this.elemY = elemY;
+    }
+    
+    
+    
+    private List getSubGroup(long x, long y){
+       
+        long a[] = new long[2];
+        long b[] = new long[2];
+        a[0] = x;
+        a[1] = y;
+        b[0] = a[0];
+        b[1] = a[1];        
+        long c[] = new long[2];
+        List l = new ArrayList();
+        
+        long start[] = new long[2];
+        
+        start[0] = Long.MIN_VALUE;
+        start[1] = Long.MIN_VALUE;
+        c = ElepticCurve.summ(b, a, curve1N, param1A, param1B);
+        while( start[0]!=c[0] || start[1]!=c[1] ){ 
+            l.add(c[0]);
+            l.add(c[1]);
+            
+            if(c[0] ==Long.MAX_VALUE && c[1] ==Long.MAX_VALUE) break;
+            if(start[0] == Long.MIN_VALUE && start[1] == Long.MIN_VALUE){
+                start[0] = c[0];
+                start[1] = c[1];
+            }
+         //   System.out.println("( "+c[0]+", "+c[1]+" )");
+            b[0] = c[0];
+            b[1] = c[1];
+   
+           c = ElepticCurve.summ(b, a, curve1N, param1A, param1B);
+           
+        
+        }
+        
+        
+        
+        
+        
+        
+        return l;
+    }
+    private List curveSqrt(HashMap sq, HashMap sqy){
+        ArrayList ymap = new ArrayList();
+        
+        Iterator sqyiter = sqy.keySet().iterator();
+//        Iterator sqiter= sq.keySet().iterator();
+      
+        while(sqyiter.hasNext()){
+            Long sqykey =  (Long) sqyiter.next();//x
+            Long sqyValue = (Long) sqy.get(sqykey.longValue()); //y^2
+            
+            Iterator sqiter = sq.keySet().iterator();
+            while(sqiter.hasNext()){
+                 Long sqkey =  (Long) sqiter.next(); //x
+                 Long sqValue = (Long) sq.get(sqkey.longValue()); //x^2
+                 
+                 if(sqyValue.equals(sqValue)){
+                   //  System.out.println("y^2: "+sqyValue+"  y: "+sqkey);
+                   //  ymap.put(sqykey.longValue(), sqkey.longValue());
+                     ymap.add(sqykey.longValue());
+                     ymap.add(sqkey.longValue());
+                 }
+                 
+            }
+            
+        }
+      
+        
+        
+        return  ymap;
+    }
+    
+    private String getCurveMapElements(List m){
+        String result = "";
+        for(int i=0; i<m.size(); i+=2){
+            if((Long)m.get(i) != Long.MAX_VALUE)
+            result+=" ( "+m.get(i) +", "+m.get(i+1) + " ) ";
+            else result+=" ( "+"INF" +", "+"INF" + " ) ";
+        }
+        
+        
+        return result;
+    }
 }
