f90dbde5f27fcada6c82248230b823f276b82bee
danon.oren@gmail.com <danon.oren@gmail.com@5ead5ee2-ed5d-3a2b-4562-3cd651b19626>
2012-12-07 20:24:25 +0000

    Added tab "Requests" Added ping servlet for maintaining validity of HTTP session. Now browser send ajax requests to this server every minute.

--- trunk/XFiles/XFiles-war/src/java/in/xfiles/web/controllers/CabinetController.java
+++ trunk/XFiles/XFiles-war/src/java/in/xfiles/web/controllers/CabinetController.java
@@ -1,13 +1,13 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package in.xfiles.web.controllers;
 
+import java.io.Serializable;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.event.ActionEvent;
-import java.io.Serializable;
+
 /**
  *
  * @author 7
@@ -15,127 +15,118 @@ import java.io.Serializable;
 @ManagedBean
 @ViewScoped
 public class CabinetController implements Serializable {
-public boolean tableVisible;
-public boolean groupsVisible;
-public boolean messagesVisible;
-public boolean profileVisible;
-public boolean historyVisible;
+
+    public static String FILES_TAB = "files";
+    public static String GROUPS_TAB = "groups";
+    public static String REQUESTS_TAB = "requests";
+    public static String MESSAGES_TAB = "messages";
+    public static String PROFILE_TAB = "profile";
+    public static String HISTORY_TAB = "history";
+    
+    
+    private Map<String, Boolean> tabs;
+
     /**
      * Creates a new instance of CabinetController
      */
     public CabinetController() {
-    }
-    
-    public void filesLinkListener(ActionEvent e){
-        
-         
-         flushFlags();
-         System.out.println("Click Files:");
-         if(tableVisible) tableVisible = false;
-         else tableVisible = true;
-         System.out.println("Visible: "+tableVisible);
-         
-    }
-    
-    public void historyLinkListener(ActionEvent e){
-        
-         
-         flushFlags();
-         System.out.println("Click History:");
-         if(historyVisible) historyVisible = false;
-         else historyVisible = true;
-         System.out.println("Visible: "+historyVisible);
-         
-    }
-    
-     public void groupsLinkListener(ActionEvent e){
-         
-         flushFlags();
-         System.out.println("Click Groups:");
-         if(groupsVisible) groupsVisible = false;
-         else groupsVisible = true;
-         System.out.println("Visible: "+groupsVisible);
-         
-    }
-     
-     public void messagesLinkListener(ActionEvent e){
-         
-         flushFlags();
-         System.out.println("Click Messages:");
-         if(messagesVisible) messagesVisible = false;
-         else messagesVisible = true;
-         System.out.println("Visible: "+messagesVisible);
-         
+        tabs = Collections.synchronizedMap(new HashMap<String, Boolean>());
+        flushFlags();
     }
 
-      public void profileLinkListener(ActionEvent e){
-         
-         flushFlags();
-         System.out.println("Click Profile:");
-         if(profileVisible) profileVisible = false;
-         else profileVisible = true;
-         System.out.println("Visible: "+profileVisible);
-         
+    public void filesLinkListener(ActionEvent e) {
+        flushFlags();
+        tabs.put(FILES_TAB, true);
+
+    }
+    
+    public void requestsLinkListener(ActionEvent e) {
+        flushFlags();
+        tabs.put(REQUESTS_TAB, true);
+    }
+
+    public void historyLinkListener(ActionEvent e) {
+        flushFlags();
+        tabs.put(HISTORY_TAB, true);
+    }
+
+    public void groupsLinkListener(ActionEvent e) {
+        flushFlags();
+        tabs.put(GROUPS_TAB, true);
+    }
+
+    public void messagesLinkListener(ActionEvent e) {
+        flushFlags();
+        tabs.put(MESSAGES_TAB, true);
+    }
+
+    public void profileLinkListener(ActionEvent e) {
+        flushFlags();
+        tabs.put(PROFILE_TAB, true);
     }
 
     public boolean isMessagesVisible() {
-        return messagesVisible;
+        return tabs.get(MESSAGES_TAB);
     }
 
     public void setMessagesVisible(boolean messagesVisible) {
-        this.messagesVisible = messagesVisible;
+        tabs.put(MESSAGES_TAB, messagesVisible);
     }
 
     public boolean isProfileVisible() {
-        return profileVisible;
+        return tabs.get(PROFILE_TAB);
     }
 
-    
     public void setProfileVisible(boolean profileVisible) {
-        this.profileVisible = profileVisible;
+        tabs.put(PROFILE_TAB, profileVisible);
     }
 
     public boolean isHistoryVisible() {
-        return historyVisible;
+        return tabs.get(HISTORY_TAB);
     }
 
     public void setHistoryVisible(boolean historyVisible) {
-        this.historyVisible = historyVisible;
+        tabs.put(HISTORY_TAB, historyVisible);
     }
-      
-      
-      
+
     public boolean isGroupsVisible() {
-        return groupsVisible;
+        return tabs.get(GROUPS_TAB);
     }
 
     public void setGroupsVisible(boolean groupsVisible) {
-        this.groupsVisible = groupsVisible;
+        tabs.put(GROUPS_TAB, groupsVisible);
     }
-    
-    private void flushFlags(){
-        groupsVisible = false;
-        tableVisible = false;
-        messagesVisible = false;
-        profileVisible = false;
-        historyVisible = false;
+
+    private void flushFlags() {
+        tabs.put(FILES_TAB, false);
+        tabs.put(GROUPS_TAB, false);
+        tabs.put(REQUESTS_TAB, false);
+        tabs.put(MESSAGES_TAB, false);
+        tabs.put(PROFILE_TAB, false);
+        tabs.put(HISTORY_TAB, false);
     }
-     
-    
-    public boolean renderUserFilesTable(){
+
+    public boolean renderUserFilesTable() {
         return true;
     }
 
-    public void clickFile(ActionEvent e){
-         System.out.println("Click File");
+    public void clickFile(ActionEvent e) {
+        System.out.println("Click File");
     }
-    
+
     public boolean isTableVisible() {
-        return tableVisible;
+        return tabs.get(FILES_TAB);
     }
 
     public void setTableVisible(boolean tableVisible) {
-        this.tableVisible = tableVisible;
+        tabs.put(FILES_TAB,tableVisible);
     }
-    
+
+    public boolean isRequestsVisible() {
+        return tabs.get(REQUESTS_TAB);
+    }
+
+    public void setRequestsVisible(boolean requestsVisible) {
+        tabs.put(REQUESTS_TAB, requestsVisible);
+    }
 }
